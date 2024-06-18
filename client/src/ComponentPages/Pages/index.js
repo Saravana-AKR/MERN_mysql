b454cd3b72178795223001acf9b80ea7d6bcd510
Saravana-AKR <erpsaro@akrind.com>
2024-06-18 19:37:00 +0530

    new change

---
+++ client/src/ComponentPages/Pages/index.js
@@ -0,0 +1,8 @@
+
+
+
+ export {default as Portal} from '../../Components/Portal';
+ export {default as Main} from '../../Components/Main';
+ export {default as Login} from '../../Components/Login';
+ export {default as Register} from '../../Components/Register';
+ export {default as User} from '../../ComponentPages/Master/User';
